0b16d5ddb193aa093e5fde47d771f030bdf62503
reinaldoperes <48915319+reinaldoperes@users.noreply.github.com>
2020-06-02 23:04:49 -0300

    commit inicial do projeto

---
+++ signalr-living-chat/SignalR-Project-2/MyConnection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace SignalR_Project_2
+{
+    public class MyConnection : PersistentConnection
+    {
+        protected override Task OnReceived(IRequest request, string connectionId, string data)
+        {
+            return Connection.Broadcast(data);
+        }
+
+        private static int QtyConnections = 0;
+
+        protected override Task OnConnected(IRequest request, string connectionId)
+        {
+            Interlocked.Increment(ref QtyConnections);
+
+            Connection.Broadcast("Clientes: " + QtyConnections);
+
+            Connection.Send(connectionId, "Bem vindo, seu id é: " + connectionId);
+
+            return base.OnConnected(request, connectionId);
+        }
+
+        protected override Task OnDisconnected(IRequest request, string connectionId, bool stopCalled)
+        {
+            Interlocked.Decrement(ref QtyConnections);
+
+            Connection.Broadcast("\nClientes: " + QtyConnections);
+
+            return base.OnDisconnected(request, connectionId, stopCalled);
+        }
+    }
+}
---
+++ signalr-living-chat/SignalR-Project-2/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalR_Project_2.Startup))]
+
+namespace SignalR_Project_2
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR<MyConnection>("/echo");
+        }
+    }
+}
